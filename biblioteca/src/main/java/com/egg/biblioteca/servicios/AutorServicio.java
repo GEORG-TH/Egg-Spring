1a6c1fc20da2dae2e489c0e3a48bbfcf741b94b8
Thierry <thierry.delacruz@hotmail.com>
2024-11-05 22:30:12 -0500

    Segundo Commit

--- biblioteca/src/main/java/com/egg/biblioteca/servicios/AutorServicio.java
+++ biblioteca/src/main/java/com/egg/biblioteca/servicios/AutorServicio.java
@@ -2,6 +2,10 @@ package com.egg.biblioteca.servicios;
 
 import com.egg.biblioteca.entidades.Autor;
 import com.egg.biblioteca.repositorio.AutorRepositorio;
+import jakarta.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +16,7 @@ public class AutorServicio {
     @Autowired
     private AutorRepositorio  autorRepositorio;
     
+    @Transactional
     public void crearAutor(String nombre){
         
         Autor autor = new Autor();
@@ -21,5 +26,24 @@ public class AutorServicio {
         autorRepositorio.save(autor);
     }
     
+    
+    public List<Autor> listarAutores(){
+        List<Autor> autores = new ArrayList<>();
+        autores = autorRepositorio.findAll();
+        return autores;
+    }
+    
+    public void modificarAutor(String id, String nombre){
+        Optional<Autor> respuestaAutor = autorRepositorio.findById(id);
+        
+        if (respuestaAutor.isPresent()) {       
+            Autor autor = respuestaAutor.get();
+            
+            autor.setNombre(nombre);
+            
+            autorRepositorio.save(autor);
+            
+        }
+    }
     
 }
--- biblioteca/src/main/java/com/egg/biblioteca/servicios/EditorialServicio.java
+++ biblioteca/src/main/java/com/egg/biblioteca/servicios/EditorialServicio.java
@@ -2,6 +2,10 @@ package com.egg.biblioteca.servicios;
 
 import com.egg.biblioteca.entidades.Editorial;
 import com.egg.biblioteca.repositorio.EditorialRepositorio;
+import jakarta.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +15,7 @@ public class EditorialServicio {
     @Autowired
     private EditorialRepositorio editorialRepositorio;
 
+    @Transactional
     public void crearEditorial(String nombre) {
 
         Editorial editorial = new Editorial();
@@ -18,6 +23,25 @@ public class EditorialServicio {
         editorial.setNombre(nombre);
 
         editorialRepositorio.save(editorial);
+    }
+    
+    public List<Editorial> listarEditoriales(){
+        List<Editorial> editoriales = new ArrayList<>();
+        editoriales = editorialRepositorio.findAll();
+        return editoriales;
+    }
+    
+     public void modificarEditorial(String id, String nombre){
+         Optional<Editorial> respuestaEditorial = editorialRepositorio.findById(id);
+        
+        if (respuestaEditorial.isPresent()) {       
+            Editorial editorial = respuestaEditorial.get();
+            
+            editorial.setNombre(nombre);
+            
+            editorialRepositorio.save(editorial);
+            
+        }
     }
 
 }
